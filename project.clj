e2272ba4d7bfcf9cf10430526e275fda5da6821b
Sunng <classicning@gmail.com>
2012-01-11 13:47:18 +0800

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject cljts "0.1.0-SNAPSHOT"
+  :description "Clojure wrapper of JTS, implements the Simple Feature Spec of Open Geospatial Consortium (OGC)."
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [com.vividsolutions/jts "1.12"]]
+  :dev-dependencies [[lein-midje "1.0.7"]
+                     [midje "1.3.1"]])
---
+++ src/cljts/core.clj
@@ -0,0 +1,1 @@
+(ns cljts.core)
