e3e1e394bd4c66a0294ba565fbf0460373849ce3
Sun Ning <classicning@gmail.com>
2012-07-31 22:27:52 +0800

    just found a workaround to use midje with lein2

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject cljts "0.2.0-SNAPSHOT"
   :description "Clojure wrapper of JTS, implements the Simple Feature Spec of Open Geospatial Consortium (OGC)."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.vividsolutions/jts "1.12"]]
+                 [com.vividsolutions/jts "1.12"]
+                 [midje "1.4.0"]]
+  :profiles {:dev {:plugins [[lein-midje "2.0.0-SNAPSHOT"]]}}
   :warn-on-reflection false)
