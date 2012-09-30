204f90f68d913a8094eff481383bb4d2342e9755
Bartosz Radaczyński <bartosz.radaczynski@tomtom.com>
2012-09-30 20:19:36 +0200

    added geojson io

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "Clojure wrapper of JTS, implements the Simple Feature Spec of Open Geospatial Consortium (OGC)."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.vividsolutions/jts "1.12"]
+                 [org.geotools/gt-geojson "8.2"]
                  [midje "1.4.0"]]
+  :repositories [["opengeo" "http://repo.opengeo.org/"]]
   :profiles {:dev {:plugins [[lein-midje "2.0.0-SNAPSHOT"]]}}
   :warn-on-reflection false)
