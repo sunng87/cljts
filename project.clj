1ba835b5ca2c0842a7c43bd700d100b9a145d4bd
Sun Ning <nsun@avos.com>
2012-10-01 18:49:14 +0800

    start next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljts "0.2.0"
+(defproject cljts "0.3.0-SNAPSHOT"
   :description "Clojure wrapper of JTS, implements the Simple Feature Spec of Open Geospatial Consortium (OGC)."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.vividsolutions/jts "1.12"]
