7fe5c7a4d23b4ef16fa6abea1e86f382c5e40f4c
Sun Ning <sunng@about.me>
2015-02-28 11:16:59 +0800

    (chore) bump dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject cljts "0.3.0-SNAPSHOT"
   :description "Clojure wrapper of JTS, implements the Simple Feature Spec of Open Geospatial Consortium (OGC)."
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.vividsolutions/jts "1.12"]
-                 [midje "1.4.0"]]
-  :profiles {:dev {:plugins [[lein-midje "2.0.0-SNAPSHOT"]]}}
-  :warn-on-reflection true)
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.vividsolutions/jts "1.13"]]
+  :profiles {:dev {:plugins [[lein-midje "3.1.3"]]
+                   :dependencies [[midje "1.6.3"]]}})
